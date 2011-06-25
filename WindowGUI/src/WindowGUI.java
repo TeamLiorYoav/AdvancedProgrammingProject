ea81cd8c204d75118677c94858fa8f31fcefcc18
Lior Kiperman <HealingAura@gmail.com>
2011-06-25 11:35:46 +0300

    WindowGUI

---
+++ WindowGUI/src/WindowGUI.java
@@ -0,0 +1,21 @@
+
+
+
+public abstract class WindowGUI {
+
+	//public ShowFiles m_ShowFiles;
+	//public UserAuthentication m_UserAuthentication;
+
+	public WindowGUI(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public abstract void UpdateWindow(String[] files);
+
+	public abstract void UpdateWindow();
+
+}
