cc60882290f0ce5a9a523a7560268cce3ca2067e
Lior Kiperman <HealingAura@gmail.com>
2011-06-25 11:41:05 +0300

    ClientGUI

---
+++ ClientGUI/src/ClientGUI.java
@@ -0,0 +1,37 @@
+
+
+
+public class ClientGUI extends WindowGUI {
+
+	//public QueriesCreation m_QueriesCreation;
+	//public DataBaseHandlerClient m_DataBaseHandlerClient;
+	//public ClientService m_ClientService;
+
+	public ClientGUI(){
+
+	}
+
+	public void finalize() throws Throwable {
+		super.finalize();
+	}
+
+    @Override
+    public void UpdateWindow(String[] files)
+    {
+    }
+
+    @Override
+    public void UpdateWindow()
+    {
+    }
+
+    /**
+	 * 
+	 * @param user
+	 */
+    /*public int ChangeServiceUser(UserInformation user)
+    {
+        return 0;
+    }*/
+
+}
